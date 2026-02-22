8f8df7d9eb9cd50017a1015efc2180beb46fcc51
Blagdon <shaun.blagdon@gmail.com>
2026-02-22 13:05:44 +0000

    Added Keystatic CMS for the committee

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react()],
+  adapter: vercel(),
 });
